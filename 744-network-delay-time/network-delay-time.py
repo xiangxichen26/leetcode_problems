6890fc23861c67a8a7f35a5ae0991cc0a81129d4
Kelly <145812636+xiangxichen26@users.noreply.github.com>
2024-07-31 22:24:57 -0700

    Time: 375 ms (46.19%) | Memory: 18.3 MB (89.96%) - LeetSync

--- 744-network-delay-time/network-delay-time.py
+++ 744-network-delay-time/network-delay-time.py
@@ -1,21 +1,31 @@
 class Solution:
     def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
-        #Bellman Ford
-
+        #SPFA
+        # create a graph
+        graph = collections.defaultdict(list)
+        for u,v,w in times:
+            graph[u].append([v,w])
+        
         #initialize a minDict array
-        minDist = [float('inf')] * n
-        minDist[k-1] = 0
+        minDist = [float('inf')] * (n+1)
+        minDist[k] = 0
+
+        queue = deque()
+        queue.append(k)
 
         # relax all the edges n-1 times
-        for _ in range(n-1):
-            for u,v,w in times:
-                if minDist[u-1] != float('inf') and minDist[u-1]+w < minDist[v-1]:
-                    minDist[v-1] = minDist[u-1]+w
+        while queue:
+            node = queue.popleft()
+            for nei,nei_w in graph[node]:
+                if minDist[node]!= float('inf') and minDist[node] + nei_w < minDist[nei]:
+                     minDist[nei] = minDist[node] + nei_w
+                     if nei not in queue:
+                        queue.append(nei)
         
         ans = -1
-        for i in minDist:
-            if i == float('inf'):
+        for i in range(1,n+1):
+            if minDist[i] == float('inf'):
                 return -1
-            if i > ans:
-                ans = i
+            if minDist[i] > ans:
+                ans = minDist[i]
         return ans
