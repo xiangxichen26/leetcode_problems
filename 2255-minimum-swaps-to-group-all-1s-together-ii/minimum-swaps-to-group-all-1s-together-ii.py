c937d715b67dbf8100723cc3fefe1776a26d1d51
Kelly <145812636+xiangxichen26@users.noreply.github.com>
2024-08-02 13:07:35 -0700

    Time: 652 ms (92.39%) | Memory: 20.6 MB (38.26%) - LeetSync

--- 2255-minimum-swaps-to-group-all-1s-together-ii/minimum-swaps-to-group-all-1s-together-ii.py
+++ 2255-minimum-swaps-to-group-all-1s-together-ii/minimum-swaps-to-group-all-1s-together-ii.py
@@ -1,33 +1,19 @@
 class Solution:
     def minSwaps(self, nums: List[int]) -> int:
-        # count the number of ones
         n = len(nums)
-        k = sum(nums)
+        total_ones = sum(nums)
         
-        # hanlde edge cases
-        if k == 0 or k == n:
+        # Edge cases
+        if total_ones == 0 or total_ones == n:
             return 0
-
-        # Extend the array to handle circular property
-        extended_nums = nums + nums[:k-1]
         
-        # Initialize the sliding window
-        current_zeros = 0
-        min_swaps = float('inf')
-        left, right = 0, 0
-
-        while right < len(extended_nums):
-            if extended_nums[right] == 0:
-                current_zeros += 1 
-            
-            if right - left + 1 >= k:
-                # 满足了窗口大小，计算min_swaps
-                min_swaps = min(min_swaps,current_zeros)
-                if extended_nums[left] == 0:
-                    current_zeros -= 1
-                left += 1
-            
-            right += 1
-
-    
-        return min_swaps
+        current_ones = sum(nums[:total_ones])
+        max_ones = current_ones
+        
+        # Use two pointers to slide the window
+        for i in range(n):
+            current_ones -= nums[i]
+            current_ones += nums[(i + total_ones) % n]
+            max_ones = max(max_ones, current_ones)
+        
+        return total_ones - max_ones
