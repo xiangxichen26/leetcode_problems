0647d43e3fcb6e3bd6255c741677c771bece95b4
Kelly <145812636+xiangxichen26@users.noreply.github.com>
2024-08-05 15:00:44 -0700

    Time: 255 ms (31.91%) | Memory: 16.8 MB (95.64%) - LeetSync

--- 1615-range-sum-of-sorted-subarray-sums/range-sum-of-sorted-subarray-sums.py
+++ 1615-range-sum-of-sorted-subarray-sums/range-sum-of-sorted-subarray-sums.py
@@ -1,17 +1,22 @@
 class Solution:
     def rangeSum(self, nums: List[int], n: int, left: int, right: int) -> int:
-        modulo = 10**9 + 7
-        all_sums = []
-        for start in range(n):
-            current_sum = 0
-            for end in range(start, n):
-                current_sum = (current_sum+nums[end]) % modulo
-                all_sums.append(current_sum)
-
-        all_sums.sort()
-
-        ans = 0
-        for i in range(left-1,right):
-            ans += all_sums[i]
+        pq = []
+        for i in range(n):
+            heapq.heappush(pq, (nums[i], i))
         
-        return ans % modulo
+        ans = 0
+        mod = 10**9 + 7
+
+        for i in range(right):
+            num, index = heapq.heappop(pq)
+
+            if i >= left - 1:
+                ans = (ans + num) % mod
+            
+            if index+1 < n:
+                sum_pair = (num + nums[index+1], index + 1)
+                heapq.heappush(pq,sum_pair)
+        
+        return ans
+
+
