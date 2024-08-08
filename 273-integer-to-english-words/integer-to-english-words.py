7e93dc6a4935d51186489736376847a8a6d2d278
Kelly <145812636+xiangxichen26@users.noreply.github.com>
2024-08-07 21:29:08 -0700

    Time: 42 ms (20.82%) | Memory: 16.7 MB (21.00%) - LeetSync

--- 273-integer-to-english-words/integer-to-english-words.py
+++ 273-integer-to-english-words/integer-to-english-words.py
@@ -2,86 +2,61 @@ class Solution:
     def numberToWords(self, num: int) -> str:
         if num == 0:
             return "Zero"
-        ones_map = {
-            1:"One",
-            2:"Two",
-            3:"Three",
-            4:"Four",
-            5:"Five",
-            6:"Six",
-            7:"Seven",
-            8:"Eight",
-            9:"Nine",
-            10:"Ten",
-            11:"Eleven",
-            12:"Twelve",
-            13:"Thirteen",
-            14:"Fourteen",
-            15:"Fifteen",
-            16:"Sixteen",
-            17:"Seventeen",
-            18:"Eighteen",
-            19:"Nineteen"
+
+        self.ones_map = {
+            1: "One", 2: "Two", 3: "Three", 4: "Four", 5: "Five",
+            6: "Six", 7: "Seven", 8: "Eight", 9: "Nine", 10: "Ten",
+            11: "Eleven", 12: "Twelve", 13: "Thirteen", 14: "Fourteen",
+            15: "Fifteen", 16: "Sixteen", 17: "Seventeen", 18: "Eighteen",
+            19: "Nineteen"
         }
 
-        tens_map = {
-            2:"Twenty",
-            3:"Thirty",
-            4:"Forty",
-            5:"Fifty",
-            6:"Sixty",
-            7:"Seventy",
-            8:"Eighty",
-            9:"Ninety"
+        self.tens_map = {
+            2: "Twenty", 3: "Thirty", 4: "Forty", 5: "Fifty",
+            6: "Sixty", 7: "Seventy", 8: "Eighty", 9: "Ninety"
         }
-        
-        suffixes = {
-            0:"", 
-            1:"Thousand",
-            2:"Million",
-            3:"Billion",
-            4:"Trillion",
-            5:"Quadrillion", 
-            6:"Quintillion",
-            7:"Sextillion",
-            8:"Septillion", 
-            9:"Octillion", 
-            10:"Nonillion",
-            11:"Decillion"
-        }
-        
+
+        self.suffixes = [
+            "", "Thousand", "Million", "Billion",
+            "Trillion", "Quadrillion", "Quintillion",
+            "Sextillion", "Septillion", "Octillion",
+            "Nonillion", "Decillion"
+        ]
+
+        words = self.convert_number_to_words(num)
+        return ' '.join(words)
+
+    def convert_number_to_words(self, num: int) -> list:
         words = []
-        count = 0 
+        count = 0
         
         while num:
             temp = num % 1000
-            num = num // 1000
+            num //= 1000
 
-            if temp == 0:
-                count += 1
-                continue
-            
-            sub_words = []
-            x = temp % 100
-            if x > 19:
-                if x % 10:
-                    sub_words.insert(0,ones_map[x % 10])
-                sub_words.insert(0,tens_map[x // 10])
-            elif x != 0:
-                sub_words.insert(0,ones_map[temp % 100])
-            
-            if temp // 100:
-                sub_words.insert(0, "Hundred")
-                sub_words.insert(0, ones_map[temp // 100])
-            
-            if count:
-                sub_words.append(suffixes[count])
-            
-            words = sub_words + words
+            if temp != 0:
+                sub_words = self.convert_chunk(temp)
+                if self.suffixes[count]:
+                    sub_words.append(self.suffixes[count])
+                words = sub_words + words
+
             count += 1
-
         
-        return ' '.join(words)
+        return words
 
-            
-
+    def convert_chunk(self, num: int) -> list:
+        sub_words = []
+        
+        if num >= 100:
+            sub_words.append(self.ones_map[num // 100])
+            sub_words.append("Hundred")
+            num %= 100
+        
+        if num >= 20:
+            sub_words.append(self.tens_map[num // 10])
+            if num % 10:
+                sub_words.append(self.ones_map[num % 10])
+        elif num > 0:
+            sub_words.append(self.ones_map[num])
+        
+        return sub_words
