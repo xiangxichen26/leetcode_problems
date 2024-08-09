4dd6693559c9c6c1435a1dca52936e6b6e44d3fc
Kelly <145812636+xiangxichen26@users.noreply.github.com>
2024-08-09 13:40:49 -0700

    Time: 37 ms (80.81%) | Memory: 16.6 MB (53.54%) - LeetSync

--- 870-magic-squares-in-grid/magic-squares-in-grid.py
+++ 870-magic-squares-in-grid/magic-squares-in-grid.py
@@ -1,46 +1,41 @@
 class Solution:
     def numMagicSquaresInside(self, grid: List[List[int]]) -> int:
-        # edge case
-        rows = len(grid)
-        cols = len(grid[0])
+        def isMagic(i, j):
+            once=[False]*10
+            rowSum=[0]*3
+            colSum= [0]*3
+
+            for a in range(i-1, i+2):
+                for b in range(j-1, j+2):
+                    x= grid[a][b]
+                    if x < 1 or x > 9: return False
+                    rowSum[a-i+1] += x
+                    colSum[b-j+1] += x
+                    if once[x]: 
+                        return False  # it's not unique
+                    once[x]=True
+
+            for b in once[1:]: 
+                if not b: return False
+
+            for sum in rowSum:
+                if sum!=15: return False
+            for sum in colSum:
+                if sum!=15: return False
+            
+            return grid[i-1][j-1]+grid[i+1][j+1]==10 and grid[i+1][j-1]+grid[i-1][j+1]==10
         
-        if rows < 3 or cols < 3:
+        r, c = len(grid), len(grid[0])
+        if r < 3 or c < 3: 
             return 0
 
-        def helper(r,c) -> int:
-            # Ensure 1-9
-            values = set()
-            for i in range(r,r+3):
-                for j in range(c, c+3):
-                    if (grid[i][j] in values) or not (1 <= grid[i][j] <= 9):
-                        return 0
-                    values.add(grid[i][j])
-            
-            # Calcuate the Rows
-            for i in range(r, r+3):
-                if sum(grid[i][c:c+3]) != 15:
-                    return 0 
+        cnt=0
+        for i in range(1, r-1):
+            for j in range(1, c-1):
+                if grid[i][j] == 5 and isMagic(i, j): 
+                    cnt+=1
+        return cnt
 
-            # Calcuate the Cols
-            for j in range(c, c+3):
-                if grid[r][j]+grid[r+1][j]+grid[r+2][j] != 15:
-                    return 0
 
-            # Calcuate the Diagonals
-            if (
-                grid[r][c] + grid[r+1][c+1]+ grid[r+2][c+2] != 15 or 
-                grid[r][c+2] + grid[r+1][c+1]+ grid[r+2][c] != 15):
-                return 0
-            
-            return 1
 
-        ans = 0
-        for i in range(rows - 2):
-            for j in range(cols - 2):
-                ans = ans + helper(i,j)
         
-        return ans
-
-
-
-
