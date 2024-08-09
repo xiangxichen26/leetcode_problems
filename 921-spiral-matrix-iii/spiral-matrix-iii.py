2f56cb6207d61aa309066348f9cde0801639075c
Kelly <145812636+xiangxichen26@users.noreply.github.com>
2024-08-08 17:08:53 -0700

    Time: 78 ms (75.37%) | Memory: 17.8 MB (38.24%) - LeetSync

---
+++ 921-spiral-matrix-iii/spiral-matrix-iii.py
@@ -0,0 +1,27 @@
+class Solution:
+    def spiralMatrixIII(self, rows: int, cols: int, rStart: int, cStart: int) -> List[List[int]]:
+        # Edge Case
+        if rows == 0 and cols == 0:
+            return []
+        
+        directions = [[0,1],[1,0],[0,-1],[-1,0]] 
+        res = []
+        r,c = rStart,cStart
+        i = 0
+        steps = 1
+
+        while len(res) < rows*cols:
+            for _ in range(2):
+                dr,dc = directions[i]
+                for _ in range(steps):
+                    if 0 <= r < rows and 0 <= c < cols:
+                        res.append([r,c])
+                    r,c = r+dr, c+dc
+                i = (i+1) % 4
+            steps += 1
+
+        return res
+        
+
+
+        
