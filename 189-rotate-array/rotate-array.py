34f8181775eec1e21406fb7a447494658fee1e2f
Kelly <145812636+xiangxichen26@users.noreply.github.com>
2024-10-04 23:04:32 -0700

    Time: 155 ms (71.93%) | Memory: 28.2 MB (7.87%) - LeetSync

--- 189-rotate-array/rotate-array.py
+++ 189-rotate-array/rotate-array.py
@@ -5,4 +5,13 @@ class Solution:
         """
         n = len(nums)
         k = k % n
-        nums[:] = nums[-k:] + nums[:-k]
+
+        def reverseArray(left,right) -> List[int]:
+            while left < right:
+                nums[left], nums[right] = nums[right], nums[left]
+                left +=1
+                right -=1
+        
+        reverseArray(0,n-1)
+        reverseArray(0,k-1)
+        reverseArray(k,n-1)
