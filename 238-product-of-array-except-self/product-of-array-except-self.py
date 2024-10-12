8b893525b047b0cce595cc1b5a92a71b0d1a9519
Kelly <145812636+xiangxichen26@users.noreply.github.com>
2024-10-12 13:33:16 -0700

    Time: 326 ms (5.25%) | Memory: 26.1 MB (34.56%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -15,143 +15,165 @@ class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         work = [0]
         ans = []
-        # Uncomment the algorithm you want to test:
-        if False:  # Change to True to activate Brute Force
-            b = L0238(nums, ans, work, "Brute Force")
-        if False:  # Change to True to activate Use Division
-            b = L0238(nums, ans, work, "Use Division")
-        if False:  # Change to True to activate n time n space
-            b = L0238(nums, ans, work, "n time n space")
-        if True:  # Change to True to activate n time 1 space
-            b = L0238(nums, ans, work, "n time 1 space")
+        if (False):
+            b = L0238(nums,ans,work,"Brute Force")
+        if (True):
+            b = L0238(nums,ans,work,"Use Division")
+        if (False):
+            b = L0238(nums,ans,work,"n time n space")
+        if (False):
+            b = L0238(nums,ans,work,"n time 1 space")
         return ans
 
 ############################################################
 # You cannot change anything in L0238
 ###########################################################
 class L0238:
-    def __init__(self, a: List[int], ans: List[int], work: 'list of size 1', alg: 'string') -> None:
-        self._a = a  # Data
-        self._ans = ans  # answer
+    def __init__(self,a: List[int],ans: List[int], work:'list of size 1', alg:'string') -> None:
+        self._a = a ## Data
+        self._ans = ans ## answer
         self._work = work
         self._l = len(a)
         for i in range(self._l):
-            self._ans.append(0)  # Data is allocated and initialized to 0
-        Algorithm(self, alg)
-    def __len__(self):
-        return self._l
+            self._ans.append(0) ## Data is allocated and initialized to 0
+        Algorithm(self,alg)
 
-    def getdata(self, i: 'int') -> 'int':
-        assert i >= 0 and i < self._l
-        self._work[0] = self._work[0] + 1
+    def __len__(self):
+        return self._l;
+
+    def getdata(self,i:'int')->'int':
+        assert(i >= 0 and i < self._l)
+        self._work[0] = self._work[0]+1 
         return self._a[i]
 
-    def getans(self, i: 'int') -> 'int':
-        assert i >= 0 and i < self._l
-        self._work[0] = self._work[0] + 1
+    def getans(self,i:'int')->'int':
+        assert(i >= 0 and i < self._l)
+        self._work[0] = self._work[0]+1 
         return self._ans[i]
 
-    def setans(self, i: 'int', v: 'int') -> 'None':
-        assert i >= 0 and i < self._l
-        self._work[0] = self._work[0] + 1
-        self._ans[i] = v
-
+    def setans(self,i:'int',v:'int')->'None':
+        assert(i >= 0 and i < self._l)
+        self._work[0] = self._work[0]+1 
+        self._ans[i] = v 
 
 class Algorithm:
-    def __init__(self, s: 'L0238', alg: 'string') -> None:
+    def __init__(self,s:'L0238', alg:'string') -> None: 
         self._s = s
-        if alg == "Brute Force":
+        if (alg == "Brute Force"):
             self._nsquare_time_constant_space()
-        elif alg == "Use Division":
-            self._n_time_constant_space_with_division()
-        elif alg == "n time n space":
+        elif (alg == "Use Division"):
+            self._n_time_constant_space_with_divison()
+        elif (alg == "n time n space"):
             self._n_time_n_space()
-        elif alg == "n time 1 space":
+        elif (alg == "n time 1 space"):
             self._n_time_1_space()
         else:
-            assert False
+            assert(False)
 
+    ########################################
+    # WRITE CODE BELOW
+    #########################################
+   
     ########################################
     # TIME:THETA(NSQUARE)
     # Space:THETA(1)
     #########################################
+    
     def _nsquare_time_constant_space(self):
         n = len(self._s)
         for i in range(n):
-            product = 1
+            p=1
             for j in range(n):
-                if i != j:
-                    product *= self._s.getdata(j)
-            self._s.setans(i, product)
+                if (i!=j):
+                    p*=self._s.getdata(j)
+                self._s.setans(i,p)
+        
 
     ########################################
     # TIME:THETA(N)
     # Space:THETA(1)
     #########################################
-    def _n_time_constant_space_with_division(self) -> 'None':  # Fixed spelling of 'division'
+    
+    def _get_product_of_whole_array(self)->'[int,bool]':
         n = len(self._s)
-        total_product = 1
-        zero_count = 0
-        zero_index = -1
-
+        p = 1
+        numzero = 0
         for i in range(n):
-            num = self._s.getdata(i)
-            if num == 0:
-                zero_count += 1
-                zero_index = i
+            v = self._s.getdata(i)
+            if (v):
+                p = p*v
             else:
-                total_product *= num
+                numzero = numzero + 1
+        
+        if(numzero == n):
+            return [0, True] 
+        
+        if(numzero == 1):
+            return [p, True]
+        
+        if(numzero > 1):
+            return [0, True]
+        
+        return [p, False]
 
-        if zero_count > 1:
-            for i in range(n):
-                self._s.setans(i, 0)
-        elif zero_count == 1:
-            for i in range(n):
-                self._s.setans(i, 0 if i != zero_index else total_product)
-        else:
-            for i in range(n):
-                self._s.setans(i, total_product // self._s.getdata(i))
+    def _n_time_constant_space_with_divison(self)->'None':
+        n = len(self._s)
+        [product, hasZero] = self._get_product_of_whole_array()
+        n = len(self._s)
+        for i in range(n):
+            if(product == 0):
+                self._s.setans(i,0)
+            else:
+                v = self._s.getdata(i)
+                if(hasZero):
+                    if(v):
+                        self._s.setans(i,0)
+                    else:
+                        self._s.setans(i,product)
+                else:
+                    if(v):
+                        self._s.setans(i, product//v)
+                    else:
+                        self._s.setans(i,0)
+
+        
+            
 
     ########################################
     # TIME:THETA(N)
     # Space:THETA(N)
     #########################################
-    def _n_time_n_space(self) -> 'None':
+    def _n_time_n_space(self)->'None':
         n = len(self._s)
-        left_products = [1] * n
-        right_products = [1] * n
-
-        for i in range(1, n):
-            left_products[i] = left_products[i - 1] * self._s.getdata(i - 1)
-
-        for i in range(n - 2, -1, -1):
-            right_products[i] = right_products[i + 1] * self._s.getdata(i + 1)
-
+        a1=[0]*n
+        a2=[0]*n
+        l=1
+        a1[0]=1
+        for i in range(1,n):
+            l*=self._s.getdata(i-1)
+            a1[i]=l
+        r=1
+        a2[n-1]=1
+        for i in range(n-2,-1,-1):
+            r*=self._s.getdata(i+1)
+            a2[i]=r
         for i in range(n):
-            self._s.setans(i, left_products[i] * right_products[i])
+            self._s.setans(i,a1[i]*a2[i])
+        
 
     ########################################
     # TIME:THETA(N)
     # Space:THETA(1)
     #########################################
-        ########################################
-    # TIME:THETA(N)
-    # Space:THETA(1)
-    #########################################
-    def _n_time_1_space(self) -> 'None':
-        n = len(self._s)
-        # Initialize the answer array with 1s
-        for i in range(n):
-            self._s.setans(i, 1)
-
-        # Calculate left products
-        left_product = 1
-        for i in range(n):
-            self._s.setans(i, left_product)
-            left_product *= self._s.getdata(i)
-
-        # Calculate right products and update answer
-        right_product = 1
-        for i in range(n - 1, -1, -1):
-            self._s.setans(i, self._s.getans(i) * right_product)
-            right_product *= self._s.getdata(i)
+    def _n_time_1_space(self)->'None':
+        n = len(self._s) 
+        p=1
+        self._s.setans(0,1)
+        self._s.setans(1,self._s.getdata(0))
+        for i in range(2,n):
+            self._s.setans(i,self._s.getans(i-1)*self._s.getdata(i-1)) 
+        temp=self._s.getdata(n-1)
+        for i in range(n-2,-1,-1):
+            self._s.setans(i,self._s.getans(i)*temp) 
+            temp*=self._s.getdata(i)
+        
