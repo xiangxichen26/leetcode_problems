ed617b955bb70f4e1e80605d36f80a62601d129f
Kelly <145812636+xiangxichen26@users.noreply.github.com>
2024-10-19 12:14:17 -0700

    Time: 0 ms (100.00%) | Memory: 17 MB (6.00%) - LeetSync

--- 225-implement-stack-using-queues/implement-stack-using-queues.py
+++ 225-implement-stack-using-queues/implement-stack-using-queues.py
@@ -1,498 +1,201 @@
-
-############################################################
-
-# Write code in file solution.py
-
-# Post solution.py in Canvas along with 4 screen shots that shows Leetcode has�
-
-#passed. We will not use screen shot to give 100
-
-# Cut and paste the whole solution.py file in Leetcode and run. All tests must pass
-
-# Note that you should do 4 times in 225, 235,622 and 641
-
-# TA will run solution.py file 4 times in 225, 235,622 and 641
-
-# All tests must pass for 100
-
-###########################################################
-
 class ListNode:
-
     # NOTHING CAN BE CHANGED HERE
-
     def __init__(self, val=0, next=None):
-
         self.val = val
-
         self.next = next
 
-
-
 ############################################################
-
-# class Slist
-
+#  class  Slist
 ###########################################################
-class Slist:
-
+class Slist():
     def __init__(self):
+        # NOTHING CAN BE CHANGED HERE
+        self._first = None
+        self._last = None
+        self._len = 0
 
-        self._head = None
+    #############################
+    # WRITE All public functions BELOW
+    # YOU CAN HAVE ANY NUMBER OF PRIVATE FUNCTIONS YOU WANT
+    #############################
 
-        self._tail = None
-
-        self._size = 0
-
-
-
-    def push(self, val):
-
+    def append_node(self, val):
         new_node = ListNode(val)
-
-        if self._head is None:
-
-            self._head = self._tail = new_node
-
+        if self._len == 0:
+            self._first = new_node
+            self._last = new_node
         else:
+            self._last.next = new_node
+            self._last = new_node
+        self._len += 1
 
-            new_node.next = self._head
+    def insert_front_node(self, val):
+        new_node = ListNode(val)
+        if self._len == 0:
+            self._first = new_node
+            self._last = new_node
+        else:
+            new_node.next = self._first
+            self._first = new_node
+        self._len += 1
 
-            self._head = new_node
-
-        self._size += 1
-
-
-
-    def pop(self):
-
-        if self._head is None:
-
+    def remove_first(self):
+        if self._len == 0:
             return None
+        removed_value = self._first.val
+        self._first = self._first.next
+        self._len -= 1
+        if self._len == 0:
+            self._last = None
+        return removed_value
 
-        val = self._head.val
-
-        self._head = self._head.next
-
-        self._size -= 1
-
-        if self._head is None:
-
-            self._tail = None
-
-        return val
-
-
-
-    def top(self):
-
-        return None if self._head is None else self._head.val
-
-
-
-    def push_back(self, val):
-
-        new_node = ListNode(val)
-
-        if self._head is None:
-
-            self._head = self._tail = new_node
-
-        else:
-
-            self._tail.next = new_node
-
-            self._tail = new_node
-
-        self._size += 1
-
-
-
-    def pop_front(self):
-
-        if self._head is None:
-
+    def remove_last(self):
+        if self._len == 0:
             return None
-
-        val = self._head.val
-
-        self._head = self._head.next
-
-        self._size -= 1
-
-        if self._head is None:
-
-            self._tail = None
-
-        return val
-
-
-
-    def peek_front(self):
-
-        return None if self._head is None else self._head.val
-
-
-
-    def push_back_circular_queue(self, val):
-
-        new_node = ListNode(val)
-
-        if self._tail is None:
-
-            self._head = self._tail = new_node
-
-            new_node.next = self._head
-
-        else:
-
-            new_node.next = self._head
-
-            self._tail.next = new_node
-
-            self._tail = new_node
-
-        self._size += 1
-
-
-
-    def pop_front_circular_queue(self):
-
-        if self._head is None:
-
-            return False
-
-        if self._head == self._tail:
-
-            self._head = self._tail = None
-
-        else:
-
-            self._tail.next = self._head.next
-
-            self._head = self._head.next
-
-        self._size -= 1
-
-        return True
-
-
-
-    def insert_front_Circular_Deque(self, val):
-
-        new_node = ListNode(val)
-
-        if self._head is None:
-
-            self._head = self._tail = new_node
-
-        else:
-
-            new_node.next = self._head
-
-            self._head = new_node
-
-        self._size += 1
-
-
-
-    def insert_last_Circular_Deque(self, val):
-
-        new_node = ListNode(val)
-
-        if self._tail is None:
-
-            self._head = self._tail = new_node
-
-        else:
-
-            self._tail.next = new_node
-
-            self._tail = new_node
-
-        self._size += 1
-
-
-
-    def delete_front_Circular_Deque(self):
-
-        if self._head is None:
-
-            return False
-
-        if self._head == self._tail:
-
-            self._head = self._tail = None
-
-        else:
-
-            self._head = self._head.next
-
-        self._size -= 1
-
-        return True
-
-
-
-    def delete_last_Circular_Deque(self):
-
-        if self._head is None:
-
-            return False
-
-        if self._head == self._tail:
-
-            self._head = self._tail = None
-
-        else:
-
-            current = self._head
-
-            while current.next != self._tail:
-
-                current = current.next
-
-            current.next = None
-
-            self._tail = current
-
-        self._size -= 1
-
-        return True
-
-
-
-    def get_front(self):
-
-        return -1 if self._head is None else self._head.val
-
-
-
-    def get_rear(self):
-
-        return -1 if self._tail is None else self._tail.val
-
-
-
-    def length(self):
-
-        return self._size
-
-
+        if self._len == 1:
+            return self.remove_first()
+
+        current = self._first
+        while current.next != self._last:
+            current = current.next
+        removed_value = self._last.val
+        self._last = current
+        self._last.next = None
+        self._len -= 1
+        return removed_value
+
+    def get_first(self):
+        return self._first.val if self._first else None
+
+    def get_last(self):
+        return self._last.val if self._last else None
+
+    def get_size(self):
+        return self._len
 
     def is_empty(self):
-
-        return self._size == 0
-
+        return self._len == 0
 
 
 ############################################################
-
-# class MyStack
-
-###########################################################
-
+#  class  MyStack
+# 225. Implement Stack using Queues
+# https://leetcode.com/problems/implement-stack-using-queues
+########################################################### 
 class MyStack:
-
     def __init__(self):
+        # NOTHING CAN BE CHANGED HERE
+        self._s = Slist()
 
-        self._stack = Slist()
+    def push(self, x):
+        self._s.append_node(x)
 
+    def pop(self):
+        return self._s.remove_last()
 
+    def top(self):
+        return self._s.get_last()
 
-    def push(self, x: int) -> None:
-
-        self._stack.push(x)
-
-
-
-    def pop(self) -> int:
-
-        return self._stack.pop()
-
-
-
-    def top(self) -> int:
-
-        return self._stack.top()
-
-
-
-    def empty(self) -> bool:
-
-        return self._stack.is_empty()
-
+    def empty(self):
+        return self._s.is_empty()
 
 
 ############################################################
-
-# class MyQueue
-
-###########################################################
-
+#  class  MyQueue
+# 232. Implement Queue using Stacks
+# https://leetcode.com/problems/implement-queue-using-stacks/
+########################################################### 
 class MyQueue:
-
     def __init__(self):
+        # NOTHING CAN BE CHANGED HERE
+        self._s = Slist()
 
-        self._queue = Slist()
+    def push(self, x):
+        self._s.append_node(x)
 
+    def pop(self):
+        return self._s.remove_first()
 
+    def peek(self):
+        return self._s.get_first()
 
-    def push(self, x: int) -> None:
-
-        self._queue.push_back(x)
-
-
-
-    def pop(self) -> int:
-
-        return self._queue.pop_front()
-
-
-
-    def peek(self) -> int:
-
-        return self._queue.peek_front()
-
-
-
-    def empty(self) -> bool:
-
-        return self._queue.is_empty()
-
+    def empty(self):
+        return self._s.is_empty()
 
 
 ############################################################
-
-# class MyCircularQueue
-
-###########################################################
-
+#  MyCircularQueue
+# 622. Design Circular Queue
+# https://leetcode.com/problems/design-circular-queue/
+########################################################### 
 class MyCircularQueue:
-
     def __init__(self, k: int):
+        # NOTHING CAN BE CHANGED HERE
+        self._K = k
+        self._s = Slist()
 
-        self._capacity = k
+    def enQueue(self, value):
+        if self._s.get_size() < self._K:
+            self._s.append_node(value)
+            return True
+        return False
 
-        self._circular_queue = Slist()
+    def deQueue(self):
+        if not self._s.is_empty():
+            self._s.remove_first()
+            return True
+        return False
 
+    def Front(self):
+        return self._s.get_first() if not self._s.is_empty() else -1
 
+    def Rear(self):
+        return self._s.get_last() if not self._s.is_empty() else -1
 
-    def enQueue(self, value: int) -> bool:
+    def isEmpty(self):
+        return self._s.is_empty()
 
-        if self.isFull():
-
-            return False
-
-        self._circular_queue.push_back_circular_queue(value)
-
-        return True
-
-
-
-    def deQueue(self) -> bool:
-
-        return self._circular_queue.pop_front_circular_queue()
-
-
-
-    def Front(self) -> int:
-
-        return self._circular_queue.get_front()
-
-
-
-    def Rear(self) -> int:
-
-        return self._circular_queue.get_rear()
-
-
-
-    def isEmpty(self) -> bool:
-
-        return self._circular_queue.is_empty()
-
-
-
-    def isFull(self) -> bool:
-
-        return self._circular_queue.length() == self._capacity
-
+    def isFull(self):
+        return self._s.get_size() == self._K
 
 
 ############################################################
-
-# class MyCircularDeque
-
-###########################################################
-
+#  MyCircularDeque
+# 641. Design Circular Deque
+# https://leetcode.com/problems/design-circular-deque
+###########################################################  
 class MyCircularDeque:
-
     def __init__(self, k: int):
+        # NOTHING CAN BE CHANGED HERE
+        self._K = k
+        self._s = Slist()
 
-        self._capacity = k
+    def insertFront(self, value):
+        if self._s.get_size() < self._K:
+            self._s.insert_front_node(value)
+            return True
+        return False
 
-        self._deque = Slist()
+    def insertLast(self, value):
+        if self._s.get_size() < self._K:
+            self._s.append_node(value)
+            return True
+        return False
 
+    def deleteFront(self):
+        return self._s.remove_first() is not None
 
+    def deleteLast(self):
+        return self._s.remove_last() is not None
 
-    def insertFront(self, value: int) -> bool:
+    def getFront(self):
+        return self._s.get_first() if not self._s.is_empty() else -1
 
-        if self.isFull():
+    def getRear(self):
+        return self._s.get_last() if not self._s.is_empty() else -1
 
-            return False
+    def isEmpty(self):
+        return self._s.is_empty()
 
-        self._deque.insert_front_Circular_Deque(value)
-
-        return True
-
-
-
-    def insertLast(self, value: int) -> bool:
-
-        if self.isFull():
-
-            return False
-
-        self._deque.insert_last_Circular_Deque(value)
-
-        return True
-
-
-
-    def deleteFront(self) -> bool:
-
-        return self._deque.delete_front_Circular_Deque()
-
-
-
-    def deleteLast(self) -> bool:
-
-        return self._deque.delete_last_Circular_Deque()
-
-
-
-    def getFront(self) -> int:
-
-        return self._deque.get_front()
-
-
-
-    def getRear(self) -> int:
-
-        return self._deque.get_rear()
-
-
-
-    def isEmpty(self) -> bool:
-
-        return self._deque.is_empty()
-
-
-
-    def isFull(self) -> bool:
-
-        return self._deque.length() == self._capacity
-
-
-
+    def isFull(self):
+        return self._s.get_size() == self._K
