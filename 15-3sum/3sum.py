6263cc5dde5476791f982a5ae953d5723742d7ba
Kelly <145812636+xiangxichen26@users.noreply.github.com>
2025-02-02 23:40:56 -0800

    Time: 791 ms (27.96%) | Memory: 21.3 MB (13.42%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -1,41 +1,46 @@
 class Solution:
-    def twoSumTarget(self, nums, start, target):
-        lo = start
-        hi = len(nums) - 1
+    # n 填写想求的是几数之和，start 从哪个索引开始计算（一般填 0），target 填想凑出的目标和
+    def nSumTarget(self, nums: List[int], n: int, start: int, target: int) -> List[List[int]]:
+        size = len(nums)
         res = []
-
-        while lo < hi:
-            s = nums[lo] + nums[hi]
-            left = nums[lo]
-            right = nums[hi]
-
-            if s < target:
-                while lo < hi and nums[lo] == left:
-                    lo += 1
-            elif s > target:
-                while lo < hi and nums[hi] == right:
-                    hi -= 1   
-            else:
-                res.append([left, right])
-                while lo < hi and nums[lo] == left: 
-                    lo += 1
-                while lo < hi and nums[hi] == right: 
-                    hi -= 1
+        if n < 2 or size < n:
+            return res
+        
+        if n == 2:
+            lo, hi = start, size - 1
+            while lo < hi:
+                left, right = nums[lo], nums[hi]
+                s = left + right
+                if s < target:
+                    while lo < hi and nums[lo] == left:
+                        lo += 1
+                elif s > target:
+                    while lo < hi and nums[hi] == right:
+                        hi -= 1
+                else:
+                    res.append([left,right])
+                    while lo < hi and nums[lo] == left:
+                        lo += 1
+                    while lo < hi and nums[hi] == right:
+                        hi -= 1
+        else:
+            # n > 2 时，递归计算 (n-1)Sum 的结果
+            for i in range(start, size):
+                # 跳过重复值
+                if i > start and nums[i] == nums[i-1]:
+                    continue
+                sub = self.nSumTarget(nums, n-1, i+1, target-nums[i])
+                for arr in sub:
+                    arr.append(nums[i])
+                    res.append(arr)
+        
         return res
+
 
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         nums.sort()
-        n = len(nums)
-        res = []
-        for i in range(n):
-            if i > 0 and nums[i] == nums[i - 1]: 
-                continue
-            tuples = self.twoSumTarget(nums, i + 1, 0 - nums[i])
+        return self.nSumTarget(nums, 3, 0, 0)
 
-            for tuple in tuples:
-                tuple.append(nums[i])
-                res.append(tuple)
-        return res
 
 
         
