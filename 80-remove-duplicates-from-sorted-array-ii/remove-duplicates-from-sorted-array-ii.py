ffebce43458534ea87e5f38beaa92cd7c00e185a
Kelly <145812636+xiangxichen26@users.noreply.github.com>
2025-02-03 22:46:30 -0800

    Time: 85 ms (5.29%) | Memory: 20.6 MB (16.73%) - LeetSync

--- 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.py
+++ 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.py
@@ -3,28 +3,20 @@ class Solution:
         slow = 0
         fast = 1
         count = 0
-        n = len(nums) 
-
-        if n <= 2:
-            return n
-        
 
         while fast < len(nums):
             if nums[fast] != nums[slow]:
-                # store the element
                 slow += 1
                 nums[slow] = nums[fast]
                 count = 0
             else:
                 if count < 2:
-                    # store the element
                     slow += 1
                     nums[slow] = nums[fast]
-                    # count = 2
                     count = 2
-            fast += 1 
-        
+            fast += 1
         return slow + 1
+            
 
 
 
