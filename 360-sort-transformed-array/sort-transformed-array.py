f872f71507f4fcc00c51606fa3a5ece03c822138
Kelly <145812636+xiangxichen26@users.noreply.github.com>
2025-02-06 20:18:55 -0800

    Time: 0 ms (100.00%) | Memory: 18 MB (12.29%) - LeetSync

--- 360-sort-transformed-array/sort-transformed-array.py
+++ 360-sort-transformed-array/sort-transformed-array.py
@@ -1,12 +1,34 @@
 class Solution:
     def sortTransformedArray(self, nums: List[int], a: int, b: int, c: int) -> List[int]:
-        res = []
+        left = 0
+        right = len(nums) - 1
+        res = [0] * len(nums)
+        p = len(nums) - 1 if a > 0 else 0
+        print(p)
 
-        for i in nums:
-            temp = a *(i*i) + b*i + c
-            res.append(temp)
-        
-        res.sort()
-
+        while left <= right:
+            # 开口向上，两端的端点一定有一个时最大值
+            val_left = self.quadraticFunction(a,b,c,nums[left])
+            val_right = self.quadraticFunction(a,b,c,nums[right])
+            if a > 0:
+                if val_left < val_right:
+                    res[p] = val_right
+                    right -= 1
+                else:
+                    res[p] = val_left
+                    left += 1
+                p -= 1
+            else:
+                if val_left < val_right:
+                    res[p] = val_left
+                    left += 1
+                else:
+                    res[p] = val_right
+                    right -= 1
+                p += 1
         return res
-        
+
+
+    def quadraticFunction(self, a, b, c, x):
+        ans = a*(x**2) + b*x + c
+        return ans
