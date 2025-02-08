8a95b3a62621699b366921ab8c2dfa0c95d493e6
Kelly <145812636+xiangxichen26@users.noreply.github.com>
2025-02-07 21:34:57 -0800

    Time: 0 ms (100.00%) | Memory: 18 MB (79.46%) - LeetSync

--- 1386-shift-2d-grid/shift-2d-grid.py
+++ 1386-shift-2d-grid/shift-2d-grid.py
@@ -1,23 +1,16 @@
 class Solution:
     def shiftGrid(self, grid: List[List[int]], k: int) -> List[List[int]]:
         m,n = len(grid),len(grid[0])
-        res_grid = [[0]*n for _ in range(m)]
+        res = [[0]*n for _ in range(m)]
 
-        while k:
-            res_grid = [[0]*n for _ in range(m)]
-            # Step 1 Element at grid[i][j] moves to grid[i][j + 1].
-            for i in range(m):
-                res_grid[i][1:n]= grid[i][0:n-1]
+        for row in range(m):
+            for col in range(n):
+                origin_index = row * n + col
+                new_index = (origin_index + k) % (m*n)
+                i = new_index // n
+                j = new_index % n
+                res[i][j] = grid[row][col]
+        return res
 
-            # Step 2 Element at grid[i][n - 1] moves to grid[i + 1][0].
-            for i in range(m-1):
-                res_grid[i+1][0] = grid[i][n - 1]
-
-            # step 3
-            res_grid[0][0] = grid[m - 1][n - 1]
-            
-            k -= 1
-            grid = res_grid
-        
-        return grid
+                
         
