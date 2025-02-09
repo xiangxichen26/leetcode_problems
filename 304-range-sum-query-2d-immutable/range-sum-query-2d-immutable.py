a45c15ef0f12d7c7868e60b218717e8a87f7f7db
Kelly <145812636+xiangxichen26@users.noreply.github.com>
2025-02-08 16:43:21 -0800

    Time: 99 ms (80.80%) | Memory: 31.2 MB (30.31%) - LeetSync

--- 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.py
+++ 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.py
@@ -1,18 +1,19 @@
 class NumMatrix:
 
     def __init__(self, matrix: List[List[int]]):
-        self._m = len(matrix)
-        self._n = len(matrix[0])
-        self.preSum = [[0]*(self._n+1) for _ in range(self._m)]
-
-        for i in range(self._m):
-            for j in range(1,self._n+1):
-                self.preSum[i][j] = self.preSum[i][j-1] + matrix[i][j-1]
+        m = len(matrix)
+        n = len(matrix[0])
+        if m == 0 or n == 0:
+            return
+        self.preSum = [[0]*(n + 1) for _ in range(m + 1)]
+        for i in range(1,m+1):
+            for j in range(1,n+1):
+                # self.preSum[i - 1][j] + self.preSum[i][j - 1] 把 self.preSum[i - 1][j - 1]算了两遍
+                self.preSum[i][j] = (self.preSum[i - 1][j] + self.preSum[i][j - 1] +
+                                     matrix[i - 1][j - 1] - self.preSum[i - 1][j - 1])
 
     def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
-        ans = 0
-        for i in range(row1, row2+1):
-            ans += self.preSum[i][col2+1] - self.preSum[i][col1]
+        ans = self.preSum[row2 + 1][col2 + 1] - self.preSum[row1][col2+1] - self.preSum[row2+1][col1] + self.preSum[row1][col1]
         return ans
 
         
