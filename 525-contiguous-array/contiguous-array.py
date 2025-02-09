bf45c42eeaed309837a9ed8ddf0f39f570d96a68
Kelly <145812636+xiangxichen26@users.noreply.github.com>
2025-02-09 12:22:40 -0800

    Time: 107 ms (45.43%) | Memory: 23.3 MB (92.33%) - LeetSync

--- 525-contiguous-array/contiguous-array.py
+++ 525-contiguous-array/contiguous-array.py
@@ -1,25 +1,19 @@
 class Solution:
     def findMaxLength(self, nums: List[int]) -> int:
-        zero, one = 0, 0
-        diff_index = {} # count[1] - count[0] ->index
+        n = len(nums)
+        preSum = 0
+        preSum_to_index = {}
+        preSum_to_index[0] = -1
+
         res = 0
-        for i,n in enumerate(nums):
-            if n == 0 :
-                zero += 1
+        for i, num in enumerate(nums):
+            # 如果这个前缀和还没有对应的索引，说明这个前缀和第一次出现，记录下来
+            preSum += num if num==1 else -1
+            if preSum not in preSum_to_index:
+                preSum_to_index[preSum] = i
             else:
-                one += 1
-            
-            if one - zero not in diff_index:
-                diff_index[one - zero] = i
-            
-            if zero == one:
-                res = one + zero
-            else:
-                idx = diff_index[one-zero]
-                res = max(res,i - idx)
-        
+                # 这个前缀和已经出现过了，则找到一个和为 0 的子数组
+                res = max(res, i - preSum_to_index[preSum])
+            # 因为题目想找长度最大的子数组，所以前缀和索引应尽可能小
         return res
-            
-
-
         
